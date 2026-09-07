0c9b9625a9554ba447b1ff594187da0541c45432
viniciusbettoe <viniciusbetto@gmail.com>
2026-09-07 19:03:48 -0300

    Diretório de backup alterado para Downloads e nome do arquivo de backup simplificado.

--- app.js
+++ app.js
@@ -743,7 +743,8 @@ async function exportarAniversariantes() {
                 aniversariantes: aniversariantes
             }
         };
-        const json = JSON.stringify(backup, null, 4);
+        // const json = JSON.stringify(backup, null, 4);
+        const json = JSON.stringify(downloads, null, 4);
         const blob = new Blob(
             [json],
             { type: "application/json" }
@@ -752,7 +753,7 @@ async function exportarAniversariantes() {
         const a = document.createElement("a");
         const hoje = new Date().toISOString().substring(0,10);
         a.href = url;
-        a.download = `Aniversariantes_Backup_${hoje}.json`;
+        a.download = `Aniversariantes_Backup.json`;
         a.click();
         URL.revokeObjectURL(url);
     }
@@ -786,7 +787,7 @@ function obterTabela(nome){
 async function importarAniversariantes() {
     try {
         // caminho fixo do arquivo de backup
-        const response = await fetch("backup/Aniversariantes_Backup.json");
+        const response = await fetch("Downloads/Aniversariantes_Backup.json");
         const backup = await response.json();
         if (!backup.dados) {
             throw new Error("Formato inválido: não encontrei 'dados' no JSON.");
